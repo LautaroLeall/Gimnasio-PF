5fa76bb8ebf96e45008f15f01e5010942a7161f7
LautaroLeall <lautaroleal4@gmail.com>
2025-10-02 16:01:36 -0300

    responsive banner coaches

--- src/components/BannerCoaches.jsx
+++ src/components/BannerCoaches.jsx
@@ -6,16 +6,16 @@ const BannerCoaches = () => {
   return (
     <div className="banner-container">
       <div className="banner-content">
-        <h2 className="banner-title">
+        <h2 className="banner-coaches-title">
           ¡ES AHORA! <br />
           TRANSFORMÁ TU CUERPO CON <br />
           <span className="highlight">EL PLAN IDEAL PARA VOS</span>
         </h2>
-        <p className="banner-description">
+        <p className="banner-coaches-description">
           Accedé hoy mismo a cualquiera de nuestras sedes y entrená con el equipo que ya eligieron
           más de 5.000 personas en Tucumán.
         </p>
-        <button className="banner-button">
+        <button className="banner-coaches-button">
           Agenda tu entrevista gratuita con un entrenador
         </button>
       </div>
